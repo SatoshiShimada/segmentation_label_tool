10725402dd36bd57e6e5c4cf0a561cc31cfbbb61
Satoshi Shimada <mylinux1204@gmail.com>
2017-11-25 00:29:57 +0900

    change file pos

--- labeling_tool/build.sh
+++ labeling_tool/build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-PROJECT_FILE="segmentation_label_tool.pro"
+PROJECT_FILE="Labeling_tool.pro"
 
 if [ $(which qmake-qt4) ]; then
 	QMAKE="qmake-qt4"
 else
 	QMAKE="qmake"
+fi
+
+build_dir=build
+if [ ! -e $build_dir ]; then
+	mkdir $build_dir
 fi
 
 ${QMAKE} -project -o ${PROJECT_FILE}
@@ -16,6 +21,9 @@ QMAKE_CXXFLAGS += -std=c++11 \`pkg-config --cflags opencv\`
 LIBS += \`pkg-config --libs opencv\`
 QT += core gui network
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+DESTDIR=../
+OBJECTS_DIR=build
+MOC_DIR=build
 FIN
 
 ${QMAKE}
